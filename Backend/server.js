f68110ae8fd2e174b2a88874da72c46b9d502283
IshitaKhandelwal2005 <153984063+IshitaKhandelwal2005@users.noreply.github.com>
2026-06-23 14:29:17 +0530

    server.js update

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,20 @@ startReminderJob()
 app.use(express.json())
 app.use(cookieParser())
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','https://medi-connect-web.vercel.app/',https://medi-connect-redic5msz-ishitas-projects-cdb5fa95.vercel.app'];
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'https://medi-connect-web.vercel.app',
+    'https://medi-connect-redic5msz-ishitas-projects-cdb5fa95.vercel.app'
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+}));
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
