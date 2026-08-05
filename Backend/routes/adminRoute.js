39fd4029d0c4d21a3a97540ab71660714f0b7782
Ishita <153984063+IshitaKhandelwal2005@users.noreply.github.com>
2026-08-05 22:49:27 +0530

    doctor request rejection for admin

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { allDoctors, loginAdmin, appointmentsAdmin, appointmentCancel, adminDashboard, approveDoctor, refreshTokenAdmin, logoutAdmin } from '../controllers/adminController.js'
+import { allDoctors, loginAdmin, appointmentsAdmin, appointmentCancel, adminDashboard, approveDoctor, disapproveDoctor, refreshTokenAdmin, logoutAdmin } from '../controllers/adminController.js'
 import upload from '../middleware/multer.js'
 import authAdmin from '../middleware/authAdmin.js'
 import { changeAvailability } from '../controllers/doctorController.js'
@@ -13,6 +13,7 @@ adminRouter.post('/refresh-token', refreshTokenAdmin)
 adminRouter.post('/logout', logoutAdmin)
 adminRouter.post('/all-doctors', authAdmin, allDoctors)
 adminRouter.post('/approve-doctor', authAdmin, approveDoctor)
+adminRouter.post('/disapprove-doctor', authAdmin, disapproveDoctor)
 adminRouter.post('/change-availability', authAdmin, changeAvailability)
 adminRouter.get('/appointments', authAdmin, appointmentsAdmin)
 adminRouter.post('/cancel-appointment', authAdmin, appointmentCancel)
